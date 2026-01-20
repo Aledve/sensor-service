ff857e21b1c031a617944ee5e9ab8effe5b5c778
Aledve <jakile971012@gmail.com>
2026-01-20 18:11:15 -0500

    🌟refactor(main): registry new controller
    Refactor FastAPI app configuration and add CORS middleware.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,22 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
-import os
 import uvicorn
-import asyncio
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.controller import sensor_controller # Importar el nuevo controlador
 
-from config import mongo_config
-from controller.forum_controller import ForumController
+app = FastAPI(title="Sensor Service", version="1.0.0")
 
-load_dotenv()
+# Configurar CORS (Importante para que el Frontend hable con él)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app = FastAPI()
-
-asyncio.run(mongo_config.init_db())
-
-app.include_router(ForumController.router)
+# Registrar rutas
+app.include_router(sensor_controller.router)
 
 if __name__ == "__main__":
-    try:
-        host = os.getenv("UVICORN_HOST", "0.0.0.0")
-        port = int(os.getenv("UVICORN_PORT", "10103"))
-
-        print(f"Starting server on {host}:{port}...")
-        uvicorn.run(app, host=host, port=port)
-    except ValueError:
-        print("Invalid port number. Please check the UVICORN_PORT environment variable.")
+    # Correr en el puerto 10051 (o el que tengas asignado en tu arquitectura)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=10051, reload=True)
