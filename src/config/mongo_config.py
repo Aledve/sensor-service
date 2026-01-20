d19abc59367320bed35be4d306baac9c8cee5a9c
Aledve <jakile971012@gmail.com>
2026-01-20 18:03:21 -0500

    🌟refactor(service): replace Forum logic with Sensor logic (Mongo/Pydantic)
    Replaced AsyncIOMotorClient with MongoClient and updated database name.

--- src/config/mongo_config.py
+++ src/config/mongo_config.py
@@ -1,14 +1,15 @@
 import os
-from motor.motor_asyncio import AsyncIOMotorClient
-from beanie import init_beanie
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-from model.forum_model import Forum
+load_dotenv()
 
+# Variables de entorno definidas en tu docker-compose o deployment.yml
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-MONGO_DATABASE_NAME = os.getenv("MONGO_DATABASE_NAME", "projectManagement")
+DB_NAME = os.getenv("MONGO_DB_NAME", "parking_sensors_db")
 
-async def init_db():
-    client = AsyncIOMotorClient(MONGO_URI)
-    database = client[MONGO_DATABASE_NAME]
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
 
-    await init_beanie(database, document_models=[Forum])
+def get_database():
+    return db
